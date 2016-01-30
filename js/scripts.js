d2f4720aad595dfd9eafc48fe6fcddc43eb9b6ee
James Turello <jdturello@gmail.com>
2016-01-29 16:04:03 -0800

    add functionality of the submit button click event - instantiating a pizza object from user input and calculating its cost

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,50 @@ Pizza.prototype.cost = function() {
   cents += (this.toppings.length * 75);
   return cost = '$' + (cents * .01);
 }
+
+$('document').ready(function() {
+  $('button#submit-btn').click(function(event) {
+    event.preventDefault();
+
+    var pizza_size = $("input[type='radio']:checked").val();
+    console.log(pizza_size);
+    var toppings = [];
+    $.each($('#checkboxes input:checked'), function(){
+      toppings.push($(this).val())
+    });
+
+    var pizza = new Pizza(pizza_size, toppings);
+    console.log(pizza);
+
+    var cost = pizza.cost();
+
+    console.log(cost);
+
+
+  });
+});
+
+
+/*
+// checked radio val:
+$('input[type="radio"]:checked').val();
+
+//checked checkboxes vals:
+$.each($('#checkboxes input:checked'), function() {array.push($(this).val())});
+
+
+
+for (var i=0; i< $('#checkboxes input:checked').length; i++) {arr.push($('#checkboxes input:checked').val());}
+
+
+
+
+$('input[type="checkbox"]').on('click', function() {
+    alert($(this).attr('value'));
+});
+
+var selected = [];
+$('#checkboxes input:checked').each(function() {
+    selected.push($(this).attr('value'));
+});
+*/
